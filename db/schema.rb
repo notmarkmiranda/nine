44b5b0e084a1c61087033f2813cdd3684db4a1c7
mark miranda <notmarkmiranda@gmail.com>
2018-01-07 10:02:10 -0700

    Add finalized migration to game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106025119) do
+ActiveRecord::Schema.define(version: 20180107170133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180106025119) do
     t.boolean "privated", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "finalized"
     t.index ["season_id"], name: "index_games_on_season_id"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
